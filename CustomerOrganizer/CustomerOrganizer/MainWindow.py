dcbf42ff56569f5dc6184c64e7d922a9ad0f76b1
finkage <finkage@gmail.com>
2020-01-21 23:45:58 -0800

    no message

--- CustomerOrganizer/CustomerOrganizer/MainWindow.py
+++ CustomerOrganizer/CustomerOrganizer/MainWindow.py
@@ -5,17 +5,3 @@ class Application(tkinter.Frame):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tkinter.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tkinter.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
