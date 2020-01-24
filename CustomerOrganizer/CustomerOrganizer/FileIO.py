6477d5924bfabee9f703363bea63496b9f4bfed2
Alinda <AlindaHeng@gmail.com>
2020-01-24 00:20:07 -0800

    Changed strings to use variable names instead.

--- CustomerOrganizer/CustomerOrganizer/FileIO.py
+++ CustomerOrganizer/CustomerOrganizer/FileIO.py
@@ -1,10 +1,14 @@
 import os
+from datetime import date
 
 # Save and load customer's data file.
 class FileIO():
+	DATA_FOLDER_NAME = "data"
+	FORMATTED_DATE = date.today().strftime("%m/%d/%y")
+
 	def save_file(self):
 		try:
-			os.makedirs("data")
+			os.makedirs(DATA_FOLDER_NAME)
 			print("Data folder created")
 		except FileExistsError:
 			print("Data folder already exists")  
