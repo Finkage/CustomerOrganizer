80c41ea60578558da2c5ffbb6aeaf9a8a72fa315
Alinda <AlindaHeng@gmail.com>
2020-01-25 20:31:57 -0800

    Changed string to use constant.

--- CustomerOrganizer/CustomerOrganizer/FileIO.py
+++ CustomerOrganizer/CustomerOrganizer/FileIO.py
@@ -4,6 +4,7 @@ from datetime import date
 # Save and load customer's data file.
 class FileIO():
 	DATA_FOLDER_NAME = "data"
+	DATA_FILE_NAME = "test.txt"
 	FORMATTED_DATE = date.today().strftime("%m/%d/%y")
 
 	def save_file(self):
@@ -13,7 +14,8 @@ class FileIO():
 		except FileExistsError:
 			print("Data folder already exists")  
 
-		file = open("data/test.txt", "w+")
+		file_name = DATA_FOLDER_NAME + DATA_FILE_NAME
+		file = open(file_name, "w+")
 		file.write("this is a test")
 		file.close()
 		print("Saved file")
